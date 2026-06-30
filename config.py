dcc600937fd8cca4e75f107dceda9c961826065e
ISHYA0987 <n210085@rguktn.ac.in>
2026-06-30 18:02:39 +0530

    feat: update website context

--- config.py
+++ config.py
@@ -1,13 +1,8 @@
 import os
 
-# ==========================================
-# Base Directory
-# ==========================================
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# ==========================================
-# Model Paths
-# ==========================================
 
 # Detector Model
 MODEL_PATH = os.path.join(BASE_DIR, "models", "detector")
@@ -20,15 +15,10 @@ HUMANIZER_MODEL_PATH = os.path.join(
     "flan_t5_base"
 )
 
-# ==========================================
-# Model Parameters
-# ==========================================
 
 MAX_LENGTH = 512
 
-# ==========================================
-# Labels
-# ==========================================
+
 
 LABELS = {
     0: "Human",
